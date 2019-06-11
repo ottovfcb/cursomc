8e12e884ff717f360b657c3235efdbbfe38a422a
Otto Victor <ottovfcb159@gmail.com>
2019-06-11 15:41:36 -0300

    Estado e cidade

--- src/main/java/com/otto/cursomc/CursomcApplication.java
+++ src/main/java/com/otto/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.otto.cursomc.domain.Categoria;
+import com.otto.cursomc.domain.Cidade;
+import com.otto.cursomc.domain.Estado;
 import com.otto.cursomc.domain.Produto;
 import com.otto.cursomc.repositories.CategoriaRepository;
+import com.otto.cursomc.repositories.CidadeRepository;
+import com.otto.cursomc.repositories.EstadoRepository;
 import com.otto.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -34,18 +42,28 @@ public class CursomcApplication implements CommandLineRunner {
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-
-
-
 		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
 		
 	}
 	
---
+++ src/main/java/com/otto/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.otto.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.otto.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
